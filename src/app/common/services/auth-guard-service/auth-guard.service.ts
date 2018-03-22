106c47375de40b488bcdfcfda25ee4add009ac33
Ashutosh Nayak <Ashutosh.Nayak@sc.com>
2018-03-22 23:11:40 +0530

    Authentication + Route Guards + Redirection

---
+++ src/app/common/services/auth-guard-service/auth-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../auth-service/auth.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.auth.user$.map(user => {
+      if (user) {
+        return true;
+      } else {
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+        return false;
+      }
+    });
+  }
+
+}
